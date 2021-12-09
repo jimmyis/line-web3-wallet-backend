239503dfd228bf37dd234865d19c1a2c552b9e3c
Jimmyis <me@jimmyis.com>
2021-12-09 13:49:14 +0700

    Updated CORS config to support localhost origin.

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -1,8 +1,6 @@
 const $whitelist = [
   `https://hoppscotch.io`,
-  `https://proxy.hoppscotch.io`,
-  `http://localhost:5001`,
-  `http://localhost:5002`,
+  `https://proxy.hoppscotch.io`
 ]
   
 export default function (whitelist: string[], options?: any) {
@@ -10,7 +8,12 @@ export default function (whitelist: string[], options?: any) {
     origin: (origin: string, callback: Function) => {
       const _whitelist = [ ...$whitelist, ...whitelist ]
 
-      if (!origin || _whitelist.indexOf(origin) !== -1) {
+      if (!origin
+        || _whitelist.indexOf(origin) !== -1
+        || origin.includes("://127.0.")
+        || origin.includes("://192.168.")
+        || origin.includes("://localhost")
+      ) {
         callback(null, true)
       } else {
         callback(new Error("CORS not allow for this origin"))
